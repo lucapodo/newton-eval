2e34d32f987d6d4c503c8b692b71a88a82bf205f
Joker84a <lucapodo97@gmail.com>
2024-03-20 11:53:04 +0100

    pushjs

--- pages/Evaluate.py
+++ pages/Evaluate.py
@@ -44,8 +44,6 @@ else:
     df_eval_newton_cot = tmp_dataset
 
 # df_eval_newton_cot = df_eval_newton_cot.sample(frac=1).reset_index(drop=True) #DA RIATTIVARE è LO SHUFFLE
-    
-
 # df_eval_newton_cot = pd.DataFrame(rows.data)
 index = 0
 radio_index = None
